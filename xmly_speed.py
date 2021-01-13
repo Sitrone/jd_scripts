1325a9476d5e4c54f38ad35f7424158dcc14ecdd
codergooze <sununiq@163.com>
2021-01-13 10:26:45 +0800

    Update xmly_speed.py

--- xmly_speed.py
+++ xmly_speed.py
@@ -7,7 +7,12 @@ from dateutil import tz
 
 
 def loadFileContent(downloadUrl):
-    return requests.get(downloadUrl).text
+	urls = downloadUrl.split(";")
+	if len(urls) == 1:
+		return requests.get(downloadUrl).text
+	else:
+		writeFile(requests.get(urls[1]).text, "USER_AGENTS.js")
+		return requests.get(urls[0]).text
 
 
 def writeFile(content, fileName):
